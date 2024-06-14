2af394014e27e297cfe6977998588b40e2fd8f90
HamzaGbada <hamza.gbada@gmail.com>
2024-06-14 10:37:53 +0100

    [init] apply threshold

--- dicom_image/utils/threshold.py
+++ dicom_image/utils/threshold.py
@@ -4,16 +4,20 @@ import numpy as np
 class Threshold:
     @staticmethod
     def otsu_threshold(image_data: np.ndarray) -> np.ndarray:
-        """This function calculates the Otsu thresholding of the image in the Dicom file.
-            For more information about the Otsu method see this link:
-            https://en.wikipedia.org/wiki/Otsu%27s_method
+        """
+        Applies Otsu's thresholding method to the given image data.
+
+        Otsu's method automatically determines the optimal threshold value by maximizing the between-class variance.
+        This method is particularly useful for bimodal images where the histogram of pixel intensities has two peaks.
+
+        For more information about the Otsu method, see this link:
+        https://en.wikipedia.org/wiki/Otsu%27s_method
 
         Args:
-            pixel_array: pixel_array in the Dicom file.
+            image_data (np.ndarray): The input image data as a NumPy array with pixel values ranging from 0 to 4095.
 
         Returns:
-            The return value is the threshold image.
-
+            np.ndarray: The binary thresholded image where pixel values are either 0 or 255.
         """
         # Compute histogram
         hist, bins = np.histogram(image_data.flatten(), bins=4096, range=(0, 4096))
@@ -39,10 +43,37 @@ class Threshold:
 
     @staticmethod
     def integral_image(image: np.ndarray) -> np.ndarray:
+        """
+        Computes the integral image of the given image data.
+
+        The integral image is a data structure used for quick and efficient computation of sum queries over image subregions.
+        Each element at (x, y) in the integral image is the sum of all pixels above and to the left of (x, y), inclusive.
+
+        Args:
+            image (np.ndarray): The input image data as a NumPy array.
+
+        Returns:
+            np.ndarray: The integral image as a NumPy array.
+        """
         return np.cumsum(np.cumsum(image, axis=0), axis=1)
 
     @staticmethod
     def sauvola_threshold(image_data: np.ndarray, window_size: int = 25, k: float = 0.2) -> np.ndarray:
+        """
+        Applies Sauvola's thresholding method to the given image data.
+
+        Sauvola's method calculates a dynamic threshold for each pixel based on the mean and standard deviation
+        of pixel intensities in a local neighborhood around the pixel. This method is particularly effective for images
+        with varying illumination conditions.
+
+        Args:
+            image_data (np.ndarray): The input image data as a NumPy array with pixel values ranging from 0 to 4095.
+            window_size (int, optional): The size of the local neighborhood window. Default is 25.
+            k (float, optional): The parameter k that determines the sensitivity of the threshold to variance. Default is 0.2.
+
+        Returns:
+            np.ndarray: The binary thresholded image where pixel values are either 0 or 255.
+        """
         # Padding to handle borders
         pad_size = window_size // 2
         padded_image = np.pad(image_data, pad_size, mode='reflect')
