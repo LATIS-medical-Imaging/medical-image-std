15acce0c8ba22ba570cf09baee1d2f9d73e8ecba
HamzaGbada <hamza.gbada@gmail.com>
2024-06-27 18:43:36 +0100

    [ADD] metrics

---
+++ medical_image/process/metrics.py
@@ -0,0 +1,14 @@
+from medical_image.data.image import Image
+
+
+class Metrics:
+    @staticmethod
+    def entropy(image: Image):
+        pass
+    @staticmethod
+    def joint_entorpy(image1: Image, image2: Image):
+        pass
+
+    @staticmethod
+    def mutual_information(image1: Image, image2: Image):
+        pass
