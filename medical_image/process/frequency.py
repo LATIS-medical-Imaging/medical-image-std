5186dbf7a7b7342b286d5f039f917076b050284b
HamzaGbada <hamza.gbada@gmail.com>
2024-07-06 16:24:25 +0100

    [feat: fedbs] frequency operations

---
+++ medical_image/process/frequency.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+class FrequencyOperations:
+    @staticmethod
+    def fft(input):
+        """
+        This function calculates 2-dimensional discrete Fourier Transform using Fast Fourier Transform Algorithms (FFT)
+        For more information about the Entropy this link:
+        https://en.wikipedia.org/wiki/Fast_Fourier_transform
+
+        Parameters:
+            input: 2d ndarray to process.
+
+        Returns:
+            out: complex ndarray
+
+
+        Examples:
+            >>> import numpy as np
+            >>> a = np.random.randint(0, 4095, (3,3))
+            >>> fft = PixelArrayOperation.fft(a)
+            >>> fft
+            array([[19218.    +0.j        ,  1506. +1307.69835971j,
+                     1506. -1307.69835971j],
+                   [ 1455. +2527.06212824j,  2893.5 +995.06318895j,
+                     1290.  +299.64478971j],
+                   [ 1455. -2527.06212824j,  1290.  -299.64478971j,
+                     2893.5 -995.06318895j]])
+
+        """
+        return np.fft.fft2(input)
+
+    @staticmethod
+    def inverse_fft(input):
+        """
+        This function calculates the inverse of 2-dimensional discrete Fourier Transform using Fast Fourier Transform Algorithms (FFT)
+        For more information about the Entropy this link:
+        https://en.wikipedia.org/wiki/Fast_Fourier_transform
+
+        Parameters:
+            input: 2d ndarray (it can be complex) to process.
+
+        Returns:
+            out: complex ndarray
+
+
+        Examples:
+            >>> import numpy as np
+            >>> a = np.random.randint(0, 4095, (3,3))
+            >>> ifft = PixelArrayOperation.inverse_fft(a)
+            >>> ifft
+            array([[19218.    +0.j        ,  1506. +1307.69835971j,
+                     1506. -1307.69835971j],
+                   [ 1455. +2527.06212824j,  2893.5 +995.06318895j,
+                     1290.  +299.64478971j],
+                   [ 1455. -2527.06212824j,  1290.  -299.64478971j,
+                     2893.5 -995.06318895j]])
+
+        """
+        return np.fft.ifft2(input)
