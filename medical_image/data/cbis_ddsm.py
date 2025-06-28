21356913ca3b4001d56baa5a29109193db3eee9f
HamzaGbada <hamza.gbada@gmail.com>
2025-06-28 17:51:13 +0100

    [init] dataset

---
+++ medical_image/data/cbis_ddsm.py
@@ -0,0 +1,5 @@
+from medical_image.data.medical_dataset import MedicalDataset
+
+# TODO: implemnt it with Tree check, download the required csv for train test, and Label, match with the required image
+class CbisDDsmDataset(MedicalDataset):
+    pass
